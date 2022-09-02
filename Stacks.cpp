21ff8a040085e173e1b4ab3182b9d6dd374cce83
Shikhar Rai <tonyrai426@gmail.com>
2022-09-02 09:25:17 +0700

    Commit

--- Stacks.cpp
+++ Stacks.cpp
@@ -101,6 +101,26 @@ int main()
 
 //     sortedInsert(stt,num);
 // }
+//Next Greater Element
+vector<int> nextGreaterElement(vector<int> nums1,vector<int> nums2){
+    unordered_map<int,int> mp;
+    stack<int> s;
+    int n=nums2.size();
+    vector<int> ans;
+    for(int i=n-1;i>=0;i--){
+        int ele=nums2[i];
+        while(!s.empty() && s.top()<=ele){
+            s.pop();
+        }
+        int res=(s.empty()) ? -1:s.top();
+        mp.insert({ele,res});
+        s.push(ele);
+    }
+    for(auto x:nums1){
+        ans.push_back(mp[x]);
+    }
+    return ans;
+}
 //Next Smaller Element
 // vector<int> nxtSmaller(vector<int> &arr,int n){
 //     stack<int> s;
